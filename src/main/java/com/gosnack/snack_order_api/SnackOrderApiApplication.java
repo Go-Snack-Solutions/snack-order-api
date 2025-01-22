1a68cc1c78ddea630eb8b7675212ee100c7a5937
Jonatas Carvalho <jonatas.ccp@gmail.com>
2025-01-22 19:51:51 -0300

    first commit

---
+++ src/main/java/com/gosnack/snack_order_api/SnackOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.gosnack.snack_order_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackOrderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gosnack/snack_order_api/SnackOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gosnack.snack_order_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
