0e76987af32fc4083ab5919bd7e742b5928b62b0
jojocarvalho <joncarvalho@uolinc.com>
2025-01-27 20:17:44 -0300

    fixing code smell

--- src/main/java/com/gosnack/snack_order_api/model/ItemModel.java
+++ src/main/java/com/gosnack/snack_order_api/model/ItemModel.java
@@ -1,8 +1,14 @@
 package com.gosnack.snack_order_api.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
 
 import java.time.LocalDateTime;
 
--- src/main/java/com/gosnack/snack_order_api/repository/ItemRepository.java
+++ src/main/java/com/gosnack/snack_order_api/repository/ItemRepository.java
@@ -1,11 +1,8 @@
 package com.gosnack.snack_order_api.repository;
 
 import com.gosnack.snack_order_api.model.ItemModel;
-import com.gosnack.snack_order_api.model.OrderModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
 
 @Repository
 public interface ItemRepository extends JpaRepository<ItemModel, Long> {
